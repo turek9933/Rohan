c34479a562ad10c4f46a5e966d438211c335a418
turek9933 <turek9933@gmail.com>
2025-05-27 16:33:50 +0200

    Clearing diffrent devices

--- utils/Task.tsx
+++ utils/Task.tsx
@@ -26,3 +26,44 @@ export interface Task {
     subtasks?: Subtask[];
     attachments?: Attachment[];
 }
+
+
+export function compareNames(a: Task, b: Task): number {
+  return a.title.localeCompare(b.title);
+}
+
+export function compareStartDates(a: Task, b: Task): number {
+    if (a.metadata.startDate && b.metadata.startDate) {
+      return a.metadata.startDate.getTime() - b.metadata.startDate.getTime();
+    } else if (a.metadata.startDate && !b.metadata.startDate) {
+      return -1;
+    } else if (!a.metadata.startDate && b.metadata.startDate) {
+      return 1;
+    } else {
+      return compareNames(a, b);
+    }
+}
+
+export function compareDeadlines(a: Task, b: Task): number {
+    if (a.metadata.deadline && b.metadata.deadline) {
+      return a.metadata.deadline.getTime() - b.metadata.deadline.getTime();
+    } else if (a.metadata.deadline && !b.metadata.deadline) {
+      return -1;
+    } else if (!a.metadata.deadline && b.metadata.deadline) {
+      return 1;
+    } else {
+      return compareNames(a, b);
+    }
+}
+
+export function compareRewards(a: Task, b: Task): number {
+    if (a.metadata.reward && b.metadata.reward) {
+      return a.metadata.reward - b.metadata.reward;
+    } else if (a.metadata.reward && !b.metadata.reward) {
+      return -1;
+    } else if (!a.metadata.reward && b.metadata.reward) {
+      return 1;
+    } else {
+      return compareNames(a, b);
+    }
+}
